84619f1dbb35190f7a0e69f23b65454b035aae3b
Hana Rigi <154543259+Hanarigi@users.noreply.github.com>
2024-10-10 00:14:41 +0330

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ToggleText from './ToggleText';
+
+function App(){
+    return (
+        <div>
+            <h1>
+                test
+                <ToggleText />
+            </h1>
+        </div>
+    );
+}
+
+export default App;
